5dfcb74fa594db7ff19363ed6837125639799a61
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-11-27 19:31:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidaysTo (name) {
    return `Happy ${holiday}, ${name}!`;
  }
 
- function happyCountdown (holiday , days) {
+ function holidayCountdown (holiday , days) {
    return `It's ${days} days until ${holiday}!`;
  }
